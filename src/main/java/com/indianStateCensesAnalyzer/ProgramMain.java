6d09a71ba24e5169ed232efb73561b4bd7997c59
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-20 18:34:05 +0530

    Added:-Welcome Message

---
+++ src/main/java/com/indianStateCensesAnalyzer/ProgramMain.java
@@ -0,0 +1,10 @@
+package com.indianStateCensesAnalyzer;
+
+public class ProgramMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Indian State Censes Analyzer");
+
+    }
+}
