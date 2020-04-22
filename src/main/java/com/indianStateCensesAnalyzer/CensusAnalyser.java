729f9f6d3735260bec4db9bf2ffc78f6b503ddcc
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-22 13:24:36 +0530

    Added:-Load The Indian State Census Information From Csv File

---
+++ src/main/java/com/indianStateCensesAnalyzer/CensusAnalyser.java
@@ -0,0 +1,40 @@
+package com.indianStateCensesAnalyzer;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CensusAnalyser
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Indian State Censes Analyser");
+    }
+    public Integer readFile(String filePath)
+    {
+        int count=0;
+        try
+        {
+            BufferedReader reader= Files.newBufferedReader(Paths.get(filePath));
+
+            CsvToBean<IndianStateCensesAnalyzer> csvToBean= new CsvToBeanBuilder(reader)
+                    .withType(IndianStateCensesAnalyzer.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<IndianStateCensesAnalyzer>stateCensesAnalyzerClassIterator=csvToBean.iterator();
+            while (stateCensesAnalyzerClassIterator.hasNext())
+            {
+                IndianStateCensesAnalyzer indianStateCensesAnalyzer=stateCensesAnalyzerClassIterator.next();
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+}
